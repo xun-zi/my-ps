199ea914a39ca1aaa3b82fe906d75f39a075ce31
xun-zi <xun-zi@1228304333@qq.com>
2022-11-13 18:42:16 +0800

    解决功能栏点击困难问题

--- my-ps/src/compontent/ToolBar/ToolBar.tsx
+++ my-ps/src/compontent/ToolBar/ToolBar.tsx
@@ -10,51 +10,51 @@ type ToolBar = {
 export default function ToolBar({ reback, redo, setFnKey }: ToolBar) {
 
     return (<div className='ToolBar'>
-        <div className='El'>
-            <HighlightOutlined onClick={() => setFnKey('draw')} />
+        <div className='El' onClick={() => setFnKey('draw')}>
+            <HighlightOutlined />
         </div>
-        <div className='El'>
-            <BorderOutlined onClick={() => { setFnKey('rect') }} />
+        <div className='El' onClick={() => { setFnKey('rect') }}>
+            <BorderOutlined />
         </div>
-        <div className='El'>
-            <LeftOutlined onClick={() => {
-                reback()
-            }} />
+        <div className='El' onClick={() => {
+            reback()
+        }} >
+            <LeftOutlined />
         </div >
-        <div className='El'>
-            <RightOutlined onClick={() => {
-                redo()
-            }} />
+        <div className='El' onClick={() => {
+            redo()
+        }} >
+            <RightOutlined />
         </div>
-        <div className='El'>
-            <BgColorsOutlined onClick={() => {
-                setFnKey('fill');
-            }} />
+        <div className='El' onClick={() => {
+            setFnKey('fill');
+        }} >
+            <BgColorsOutlined />
         </div>
-        <div className='El'>
-            <div onClick={() => {
-                setFnKey('straw');
-            }}>
+        <div className='El' onClick={() => {
+            setFnKey('straw');
+        }}>
+            <div >
                 吸
             </div>
         </div>
-        <div className='El'>
-            <div onClick={() => {
-                setFnKey('filter');
-            }}>
+        <div className='El' onClick={() => {
+            setFnKey('filter');
+        }}>
+            <div >
                 滤
             </div>
         </div>
-        <div className='El'>
-            <div onClick={() => {
-                setFnKey('magic');
-            }}>
+        <div className='El' onClick={() => {
+            setFnKey('magic');
+        }}>
+            <div >
                 魔
             </div>
         </div>
         <div className='El'>
             <DownloadOutlined onClick={() => setFnKey('download')} />
-            
+
         </div>
     </div>)
 }
